2f0feb0813af81110d52bb8c11566ef15e5e6270
ririringer <honnmadesu6@gmail.com>
2024-01-17 01:15:44 +0900

    修正

--- src/main.js
+++ src/main.js
@@ -28,23 +28,22 @@ navigator.serviceWorker
 
     // トークン取得とサブスクリプション処理をここで行います
     navigator.serviceWorker.ready.then(() => {
-      getToken(messaging, {
-        vapidKey:
-          "BH8GZqS8nQ__4pSOkTXit4sQcuSAHItUDr7xEWn_qeUf0SK3QOsCK0bYnC0ajh0-NoM6jmjm_eV8XrnvTsJe9is", // VAPIDキーを適切に設定
-      })
-        .then((currentToken) => {
-          if (currentToken) {
-            console.log("token:", currentToken);
-            subscribeDeviceToTopic(currentToken);
-          } else {
-            console.log(
-              "No registration token available. Request permission to generate one."
-            );
-            console.log("Requesting permission...");
-
-            Notification.requestPermission().then((permission) => {
-              if (permission === "granted") {
-                console.log("Notification permission granted.");
+      Notification.requestPermission()
+        .then((permission) => {
+          if (permission === "granted") {
+            console.log("Notification permission granted.");
+            getToken(messaging, {
+              vapidKey:
+                "BH8GZqS8nQ__4pSOkTXit4sQcuSAHItUDr7xEWn_qeUf0SK3QOsCK0bYnC0ajh0-NoM6jmjm_eV8XrnvTsJe9is", // VAPIDキーを適切に設定
+            }).then((currentToken) => {
+              if (currentToken) {
+                console.log("token:", currentToken);
+                subscribeDeviceToTopic(currentToken);
+              } else {
+                console.log(
+                  "No registration token available. Request permission to generate one."
+                );
+                console.log("Requesting permission...");
 
                 navigator.serviceWorker.ready.then((p) => {
                   p.pushManager.getSubscription().then((subscription) => {
